b20d9a2bdb919a198cf7e0180165a076c4cd15cf
DylanBirot <dylanjbirot@gmail.com>
2017-10-25 15:18:56 +1000

    Animation Fix 1

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
+
+
+
+    $(window).scroll(function() {
+        console.log($(window).scrollTop());
+        if ($(window).scrollTop() > 200) {
+            $('.nav-bg').removeClass('nav-slideBackup');
+            $('.nav-bg').addClass('nav-slideDown');
+            $('.nav-bg').css('display', 'block');
+            $('.left li a').css('color', 'black');
+            $('#top').css('background', 'black');
+            $('#bot').css('background', 'black');
+            $('.header-logo').attr('src', 'assets/icons/bonnesailogoblack.svg');
+        } else {
+            $('.nav-bg').removeClass('nav-slideDown');
+            $('.nav-bg').addClass('nav-slideBackup').delay(2000).css('display', 'none');
+            $('.left li a').css('color', 'white');
+            $('#top').css('background', 'white');
+            $('#bot').css('background', 'white');
+            $('.header-logo').attr('src', 'assets/icons/bonnesailogo.svg');
+        }
+      });
     
 
-    console.log('read');
+
     $('.header').children().css('opacity', '0');
     $('#header-tiles').css('opacity', '1');
 
@@ -18,6 +40,7 @@ $(document).ready(function() {
     }, 3500);
 
     setTimeout(function() {
+        
         $('.header h1').css('visibility', 'visible');
         $('.header hr').css('visibility', 'visible');
         $('.header p').css('visibility', 'visible');
@@ -25,7 +48,7 @@ $(document).ready(function() {
     }, 2300);
 
 
-    
+ 
 
 
     $("#scrolldown").click(function() { /*select class that triggers scroll*/
