3aa278f3c5a36a5ce4160985c8e8df15a5af7015
DylanBirot <dylanjbirot@gmail.com>
2017-12-20 16:55:51 +1000

    20 Dec 2017

--- index.js
+++ index.js
@@ -113,6 +113,21 @@ $(document).ready(function() {
 	});
 
 
-
+	jQuery(document).ready(function($) {
+		//create the slider
+		$('.cd-testimonials-wrapper').flexslider({
+			selector: ".cd-testimonials > li",
+			animation: "slide",
+			controlNav: true,
+			slideshow: false,
+			smoothHeight: true,
+			start: function() {
+				$('.cd-testimonials').children('li').css({
+					'opacity': 1,
+					'position': 'relative'
+				});
+			}
+		});
+	});
 
 });
